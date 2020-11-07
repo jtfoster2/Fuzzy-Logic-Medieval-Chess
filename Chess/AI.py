ddc3aa6c14416b7ef622626db0c834e60c9201bb
John Hurd <johnhurd04@gmail.com>
2020-11-07 13:31:22 -0700

    finished best move method. move handling to come

--- Chess/AI.py
+++ Chess/AI.py
@@ -98,45 +98,159 @@ class Corp():
         if piece[1] == "P":
             return 0
 
-    #chooses best capture and sets best_capture method of evaluting may change
+    #chooses best capture
     def bestCapture(self):
         best = ('ZZZ', 'YYY')
         best_ranked = (5, 0)
         best_diff = -6
+        best_attack_diff = 6
         if len(self.captures)>0:
             for pair in self.captures:
                 pair_ranked = (self.evaluate(pair[0]), self.evaluate(pair[1]))
                 diff = pair_ranked[1] - pair_ranked[0]
-                if diff >= best_diff:
-                    if self.mode == "retreat":
-                        if pair[0] == "R": #only archers attack while retreating
+
+                #uses weak pieces to attack equal or strong pieces in advance mode
+                if self.mode != "attack":
+                    if diff >= best_diff:
+                        if self.mode == "retreat":
+                            if pair[0] == "R": #only archers attack while retreating
+                                best = pair
+                                best_ranked = pair_ranked
+                                best_diff = diff
+                            else:
+                                pass
+                        else:    
                             best = pair
                             best_ranked = pair_ranked
                             best_diff = diff
-                        else:
-                            pass
-                    else:    
-                        best = pair
+                
+                #uses strong pieces to attack equal or weaker pieces in attack mode
+                if self.mode == "attack":
+                    if diff <= best_attack_diff:
+                        best = pair 
                         best_ranked = pair_ranked
-                        best_diff = diff
-        
+                        best_attack_diff = diff
+              
+
             move = ChessEngine.Move(self.locate(best[0]), self.locate(best[1]),self.gs.board)
             self.best_capture = move
+            print("Best Capture: " + self.identify((self.best_capture.startRow,self.best_capture.startCol)) + " takes " +self.identify((self.best_capture.endRow,self.best_capture.endCol)))
+
         else:
             self.best_capture = 0
-    
-
 
     #chooses best non-capture move
     def bestMove(self):
+        
+        #finds furthest forward move of most powerful piece excluding leaders 60 percent of the time and including leaders 40
         if self.mode=='attack':
-            pass #replace this with a heuristic calculation for the furthest forward move that does not place the piece in range of an attack other than one by an archer. Prioritize moves by pawns, knights, and the queeen
+            if len(self.moves)>0:
+                percentage = random.randint(0,100)
+
+                if percentage <= 60:
+                    #finds most powerful movable piece other than leaders
+                    pwr_piece = 0
+                    for move in self.moves:
+                        if self.evaluate(self.identify(move[0])) >= pwr_piece and self.evaluate(self.identify(move[0])) < 4:
+                            pwr_piece = self.evaluate(self.identify(move[0]))
+
+                else:
+                        #finds most powerful movable piece
+                        pwr_piece = 0
+                        for move in self.moves:
+                            if self.evaluate(self.identify(move[0])) >= pwr_piece:
+                                pwr_piece = self.evaluate(self.identify(move[0]))
+
+
+                #creates list of moves using that type of piece
+                possible_moves = []
+                for move in self.moves:
+                    if self.evaluate(self.identify(move[0])) == pwr_piece:
+                        possible_moves.append(move)
+
+                #finds furthest forward move out of the possible moves
+                if len(possible_moves)>0:
+                    if self.color == 1:
+                        max_diff = 0
+                        move = 0
+                        for possible in possible_moves:
+                            diff = possible[1][0] - possible[0][0]
+                            if diff >= max_diff:
+                                max_diff = diff
+                                move = possible
+                        if move != 0:
+                            self.best_move = ChessEngine.Move(move[0],move[1],self.gs.board)
+    
+                    if self.color == 0:
+                        max_diff = 0
+                        move = 0
+                        for possible in possible_moves:
+                            diff = possible[0][0] - possible[1][0]
+                            if diff >= max_diff:
+                                max_diff = diff
+                                move = possible
+                        if move != 0:
+                            self.best_move = ChessEngine.Move(move[0],move[1],self.gs.board)
+
+
+        #finds furthest forward move 40 percent of the time and furthest safe move 60 percent of the time (math favors pawns and knights in the beginning)
+        #40 60 rule also enable 2 AIs to actually attack each other. Otherwise, the AIs would never go into vulnerable places.
         if self.mode == 'advance':
-            pass #replace this with a heuristic calculation for the furthest forward move that does not place the piece in range of an attack other than one by an archer. prioritize moves by less important piecesother than archers.
+            percentage = random.randint(0,100)
+
+            if percentage <=60:
+                possible_moves = []
+
+                #find all moves with no adjacent opposing pieces
+                if len(self.moves) >0:
+                    for move in self.moves:
+                        empty_count = 0 #if this ends up at 8, the space has no adjacent opposing pieces
+                        end = move[1]
+                        for i in range(-1,2):
+                            for j in range(-1,2):
+                                location = (i,j)
+                                if location != (0,0):
+                                    if self.color == 1:
+                                        if self.identify(location)[0] != 'w':
+                                            empty_count = empty_count + 1
+                                    if self.color == 0:
+                                        if self.identify(location)[0] != 'b':
+                                            empty_count = empty_count + 1
+                        if empty_count == 8:
+                            possible_moves.append(move)
+            
+            else:
+                possible_moves = self.moves
+            #find move with biggest row difference
+            if len(possible_moves)>0:
+                if self.color == 1:
+                    max_diff = 0
+                    move = 0
+                    for possible in possible_moves:
+                        diff = possible[1][0] - possible[0][0]
+                        if diff >= max_diff:
+                            max_diff = diff
+                            move = possible
+                    if move != 0:
+                        self.best_move = ChessEngine.Move(move[0],move[1],self.gs.board)
+
+                if self.color == 0:
+                    max_diff = 0
+                    move = 0
+                    for possible in possible_moves:
+                        diff = possible[0][0] - possible[1][0]
+                        if diff >= max_diff:
+                            max_diff = diff
+                            move = possible
+                    if move != 0:
+                        self.best_move = ChessEngine.Move(move[0],move[1],self.gs.board)
+
+        #finds either the furthest backward movement or the furthest backward movement of the most vulnerable piece
         if self.mode =='retreat':
+
             #compute currently vunerable pieces
             for piece in self.opposing:
-            self.vmov.generate(self.locate(piece)[0],self.locate(piece)[1])
+                self.vmov.generate(self.locate(piece)[0],self.locate(piece)[1])
             for location in self.vmov.legal_attacks:
                 self.op_caps.append((piece,self.identify(location)))
             self.vmov.clearGenerated()
@@ -159,11 +273,11 @@ class Corp():
                 #handling for black team
                 if self.color == 1:
                     #compute furthest backward movement of most vulnerable piece
-                    move_loc = (-1,8)
+                    move_loc = (8,-1)
                     for move in self.moves:
-                        if move[1][1] <= move_loc[1] and move[0] == piece_loc:
+                        if move[1][0] <= move_loc[0] and move[0] == piece_loc:
                             move_loc = move[1]
-                    if move_loc[0] != -1:
+                    if move_loc[1] != -1:
                         move = ChessEngine.Move(piece_loc, move_loc,self.gs.board)
                         self.best_move = move
                     else:
@@ -174,9 +288,9 @@ class Corp():
                     #compute furthest backward movement of most vulnerable piece
                     move_loc = (-1,-1)
                     for move in self.moves:
-                        if move[1][1] >= move_loc[1] and move[0] = piece_loc:
+                        if move[1][0] >= move_loc[0] and move[0] == piece_loc:
                             move_loc = move[1]
-                    if move_loc[0] != -1:
+                    if move_loc[1] != -1:
                         move = ChessEngine.Move(piece_loc, move_loc,self.gs.board)
                         self.best_move = move
                     else:
@@ -191,7 +305,7 @@ class Corp():
                     #find highest rank
                     rank = 0
                     for i in range(0,8):
-                        for j in range(0,8:
+                        for j in range(0,8):
                             if self.gs.board[i][j][0] == 'b' and j >= rank:
                                 rank = j
                     
@@ -237,7 +351,7 @@ class Corp():
                     #compute furthest backward movement of most vulnerable piece
                     move_loc = (-1,-1)
                     for move in self.moves:
-                        if move[1][1] >= move_loc[1] and move[0] = piece_loc:
+                        if move[1][1] >= move_loc[1] and move[0] == piece_loc:
                             move_loc = move[1]
                     if move_loc[0] != -1:
                         move = ChessEngine.Move(piece_loc, move_loc,self.gs.board)
@@ -245,14 +359,7 @@ class Corp():
                     else:
                         self.best_move = 0
 
-
- 
-                        
-    
-
-
-
-    #changes mode
+    #updates pieces on the board and changes mode accordingly
     def strategize(self): #attack means focus on captures. Advance means an equal focus on captures and advancing pieces forwards. Retreat means a focus on archer captures and moving pieces backwards
         #updates pieces
         if self.color == 1:
@@ -296,6 +403,7 @@ class Corp():
                 num_of_black_dead = num_of_black_dead + 1
         diff = num_of_white_dead - num_of_black_dead
 
+        #if AI is white
         if self.color == 0:
             if diff >2:
                 self.mode ="retreat"
@@ -303,7 +411,8 @@ class Corp():
                 self.mode = "advance"
             if diff < 0:
                 self.mode = "attack"
-        
+
+        #if AI is black
         if self.color == 1:
             if diff <-2:
                 self.mode ="retreat"
@@ -425,7 +534,7 @@ class Corp():
                 print("New Turn: ", self.gs.treg.currentTurn )
 
 
-    #clears 
+    #clears values after a move
     def clear(self):
         self.moves = []
         self.captures = []
@@ -434,6 +543,7 @@ class Corp():
         self.best_capture = 0
         self.best_move = 0
 
+    #executes method sequence required to make a move
     def step(self):
         self.strategize()
         self.allMoves()
